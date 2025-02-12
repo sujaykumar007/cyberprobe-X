667f0574200f440d3f5b29ab49be22c6f379a23c
sujaykumar007 <sujayadur551@gmail.com>
2025-02-12 12:32:15 +0530

    added structure of page

--- app/page.tsx
+++ app/page.tsx
@@ -1,8 +1,12 @@
+import { About } from "./components/About";
 
-
-export default function Home(){
-  
-  return(
-    <div></div>
-  )
+export default function Home() {
+  return (
+    <div className="w-full min-h-screen flex flex-col items-center justify-center">
+      <div className="max-w-3xl text-center mb-12">
+        <h1 className="text-5xl font-bold mb-4 text-black">home component</h1>
+      </div>
+      <About />
+    </div>
+  );
 }
