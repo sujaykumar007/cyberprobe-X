5b9da99312cf255558a1811d5a3780c71d2c6690
sujaykumar <82111261+sujaykumar007@users.noreply.github.com>
2025-02-13 14:48:38 +0530

    added new section animation in home page

---
+++ app/components/velocityAnimation.tsx
@@ -0,0 +1,12 @@
+import { VelocityScroll } from "@/components/magicui/scroll-based-velocity";
+
+
+export function ScrollAnimation() {
+  return (
+    <div className="relative flex w-full flex-col items-center justify-center overflow-hidden">
+      <VelocityScroll defaultVelocity={4} numRows={2}>Penetration Testing</VelocityScroll>
+      <div className="pointer-events-none absolute inset-y-0 left-0 w-1/4 bg-gradient-to-r from-background"></div>
+      <div className="pointer-events-none absolute inset-y-0 right-0 w-1/4 bg-gradient-to-l from-background"></div>
+    </div>
+  );
+}
--- app/page.tsx
+++ app/page.tsx
@@ -1,6 +1,7 @@
 
 import { Header } from "./components/Header";
 import Testing from "./components/Testing";
+import { ScrollAnimation } from "./components/velocityAnimation";
 
 
 export default function Home(){
@@ -8,6 +9,9 @@ export default function Home(){
     <div className="w-full min-h-screen flex flex-col items-center justify-center">
       <div className="text-center mb-12 bg-blue-500 w-[95%] h-screen rounded-3xl mt-4 md:mt-8 lg:mt-20 overflow-hidden">
         <Header />
+      </div>
+      <div className="mt-10 mb-10">
+        <ScrollAnimation />
       </div>
       <div>
       <Testing />
