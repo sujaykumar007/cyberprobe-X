1ea9de6bbf5dc54e5dd649b58c96432d75ec8d4e
sujaykumar <82111261+sujaykumar007@users.noreply.github.com>
2025-02-19 16:58:46 +0530

    palced evrything

--- app/page.tsx
+++ app/page.tsx
@@ -10,20 +10,21 @@ import SwiperComponent from "./custom-components/swiper";
 export default function Home(){  
  
   return (
-    <div className="w-full max-h-full flex flex-col items-center justify-center bg-black ">
-      <div className={`text-center mb-10 w-[95%]  rounded-3xl mt-16 md:mt-8 lg:mt-20 overflow-hidden `}  style={{ backgroundColor: "var(--primary-blue)"}}>
+    <div className="w-full pb-40 max-h-full flex flex-col gap-y-40 items-center justify-center bg-black ">
+      <div className={`text-center  w-[95%]  rounded-3xl mt-16 md:mt-8 lg:mt-20 overflow-hidden `}  style={{ backgroundColor: "var(--primary-blue)"}}>
         <Header />
       </div>
-      <div className="mb-10" style={{ marginTop: -40 }}>
+      <div style={{ marginTop: -40 }}>
         <VelocityScrolling />
       </div>
       <div className="w-[100%] container">
         <WhyUs />
       </div>
-      <div>
-      <Testing />
-      </div>
-      <div>
+     
+      <div className=" flex flex-col gap-20" >
+        <h1 className="text-2xl sm:text-4xl md:text-5xl lg:text-5xl font-extrabold  text-white">
+        Comprehensive <span className="text-primary-yellow">Cybersecurity</span> Services
+        </h1>
       <SwiperComponent/>
       </div>
     </div>
