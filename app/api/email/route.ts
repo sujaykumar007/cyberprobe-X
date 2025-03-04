302df3ab35fab2742f52fe58ca09ab2662ae070f
sujaykumar <82111261+sujaykumar007@users.noreply.github.com>
2025-03-04 11:29:56 +0530

    added email template change and remove designation

--- app/api/email/route.ts
+++ app/api/email/route.ts
@@ -3,11 +3,12 @@ import emailjs from '@emailjs/nodejs'
 
 export async function POST(req: Request) {
   try {
-    const { name, email, companyName, designation, phone, message } = await req.json()
+    const { firstName, lastName, email, companyName,  phone, message } = await req.json()
 
-    if (!name || !email || !companyName || !designation || !phone || !message) {
+    if (!firstName || !lastName || !email || !companyName  || !phone ) {
       return NextResponse.json({ success: false, message: 'All fields are required.' }, { status: 400 })
     }
+
 
     if (!process.env.EMAILJS_SERVICE_ID || !process.env.EMAILJS_TEMPLATE_ID || !process.env.EMAILJS_PUBLIC_KEY || !process.env.EMAILJS_PRIVATE_KEY) {
       return NextResponse.json({ success: false, message: 'Email.js credentials are not set.' }, { status: 500 })
@@ -18,10 +19,10 @@ export async function POST(req: Request) {
       process.env.EMAILJS_TEMPLATE_ID,
       {
         to_name: 'Support Team',
-        from_name: name,
+        from_first_name: firstName,
+        from_last_name: lastName,
         from_email: email,
         company_name: companyName,
-        designation: designation,
         phone: phone,
         message: message
       },
--- app/custom-components/ContactForm.tsx
+++ app/custom-components/ContactForm.tsx
@@ -1,18 +1,17 @@
 'use client'
 
-import { useState } from 'react'
+import {  useState } from 'react'
 import { Card, CardContent } from '@/components/ui/card'
 import { Button } from '@/components/ui/button'
 import { Input } from '@/components/ui/input'
 import { Textarea } from '@/components/ui/textarea'
 
-
 export default function ContactForm() {
   const [formData, setFormData] = useState({
-    name: '',
+    firstName: '',
+    lastName: '',
     email: '',
     companyName: '',
-    designation: '',
     phone: '',
     message: '',
   })
@@ -25,6 +24,7 @@ export default function ContactForm() {
   const handleSubmit = async (e:any) => {
     e.preventDefault()
     setStatus('Sending...')
+    console.log(formData)
 
     const res = await fetch('/api/email', {
       method: 'POST',
@@ -36,29 +36,29 @@ export default function ContactForm() {
     setStatus(data.message)
 
     if (data.success) {
-      setFormData({ name: '', email: '', companyName: '', designation: '', phone: '', message: '' }) 
+      setFormData({ firstName: '', lastName: '', email: '', companyName: '',  phone: '', message: '' }) 
     }
   }
 
   return (
-    <Card className="flex flex-col md:flex-row bg-black text-white p-10 rounded-2xl shadow-lg w-full ">
-    
-      <div className="flex-1 flex flex-col justify-center p-6 space-y-4">
-
+    <Card className="flex flex-col md:flex-row bg-black text-white py-10 px-5 rounded-2xl shadow-lg w-full ">
+      <div className="flex-1 flex flex-col justify-center px-16 space-y-4">
         <p className="text-gray-300">Lorem ipsum dolor sit amet, consectetur adipiscing elit. Phasellus nec iaculis mauris.</p>
       </div>
 
-      <CardContent className="flex-1 ">
+      <CardContent className="flex-1 pt-10 md:pt-0 ">
         <form onSubmit={handleSubmit} className="space-y-6 md:w-[85%] w-full">
           {status && <p className="text-primary-yellow">{status}</p>}
 
-          <Input type="text" name="name" placeholder="Your Name" value={formData.name} onChange={handleChange} required className="bg-gray-800 py-5   text-white" />
-          <Input type="email" name="email" placeholder="Work Email" value={formData.email} onChange={handleChange} required className="bg-gray-800 py-5  text-white" />
-          <Input type="text" name="companyName" placeholder="Company Name" value={formData.companyName} onChange={handleChange} required className="bg-gray-800 py-5  text-white" />
-          <Input type="text" name="designation" placeholder="Designation" value={formData.designation} onChange={handleChange} required className="bg-gray-800 py-5  text-white" />
-          <Input type="number" name="phone" placeholder="Phone Number" value={formData.phone} onChange={handleChange} required className="bg-gray-800 py-5  text-white" />
-          <Textarea name="message" placeholder="Your Message" value={formData.message} onChange={handleChange} required className="bg-gray-800 py-5  text-white" />
-
+          <div className="flex space-x-4">
+            <Input type="text" name="firstName" placeholder="Your First Name" value={formData.firstName} onChange={handleChange} required className="bg-gray-800 py-5 text-white" />
+            <Input type="text" name="lastName" placeholder="Your Last Name" value={formData.lastName} onChange={handleChange} required className="bg-gray-800 py-5 text-white" />
+          </div>
+          <Input type="email" name="email" placeholder="Work Email" value={formData.email} onChange={handleChange} required className="bg-gray-800 py-5 text-white" />
+          <Input type="text" name="companyName" placeholder="Company Name" value={formData.companyName} onChange={handleChange} required className="bg-gray-800 py-5 text-white" />
+          <Input type="number" name="phone" placeholder="Phone Number" value={formData.phone} onChange={handleChange} required className="bg-gray-800 py-5 text-white appearance-none" />
+          <Textarea name="message" placeholder="Your Message" value={formData.message} onChange={handleChange}  className="bg-gray-800 py-5 text-white" />
+          
           <Button type="submit" className="w-full bg-primary-yellow text-black font-semibold hover:bg-primary-yellow">Send Email</Button>
         </form>
       </CardContent>
