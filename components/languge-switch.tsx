c25f863e65e7baace3f2384c4de60be101b715d1
sujaykumar <82111261+sujaykumar007@users.noreply.github.com>
2025-03-15 19:01:22 +0530

    remove transltare

--- components/languge-switch.tsx
+++ components/languge-switch.tsx
@@ -1,14 +1,7 @@
+"use client"
 
-"use client";
-
+import "../app/globals.css"
 import { useEffect } from "react";
-
-declare global {
-  interface Window {
-    google: any;
-  }
-}
-
 export default function LanguageSwitcher() {
   useEffect(() => {
     const addGoogleTranslateScript = () => {
@@ -16,13 +9,13 @@ export default function LanguageSwitcher() {
         const script = document.createElement("script");
         script.id = "google-translate-script";
         script.src = "https://translate.google.com/translate_a/element.js?cb=TranslateInit";
-
         script.async = true;
         document.body.appendChild(script);
       }
     };
 
     (window as any).TranslateInit = () => {
+      //@ts-ignore
       new window.google.translate.TranslateElement(
         {
           pageLanguage: "en",
@@ -30,6 +23,15 @@ export default function LanguageSwitcher() {
         },
         "google_translate_element"
       );
+
+      // Hide unwanted iframe after translation script loads
+      setTimeout(() => {
+        const iframe = document.querySelector("iframe.skiptranslate");
+        if (iframe) {
+          //@ts-ignore
+          iframe.style.display = "none";
+        }
+      }, 1000);
     };
 
     addGoogleTranslateScript();
