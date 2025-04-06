db214607f461deb2f01102d9ca0809eec9298fe7
sujaykumar <82111261+sujaykumar007@users.noreply.github.com>
2025-04-06 22:46:24 +0530

    add a meta

---
+++ app/assets/assets.tsx
@@ -0,0 +1,50 @@
+import logo from './cyber.jpg'
+import menu_bar from './menu.png'
+import dashbord from './Dashbord.webp'
+import whiteastra from './detect.jpg'
+import menubutton from './menu-white.png'
+import newlogo from "./cyberprobex.png"
+import logoX from "./twitter.png"
+import astra from './Dashbord.webp'
+import linkedin from "./linkedin.png"
+import instagram from "./instagram.png"
+import youtube from "./youtube.png"
+import whitelogo from './cyberprobex white.png'
+import card1 from './card1.jpg'
+import card2 from './card2.jpg'
+import card3 from './card3.jpeg'
+import card4 from './card4.jpg'
+import card5 from './card5.jpg'
+import card6 from './card6.jpg'
+
+
+
+
+export const assets={
+     logo,
+     menu_bar,
+     dashbord,
+     whiteastra,
+     newlogo,
+     menubutton,
+
+     logoX,
+      astra,
+      whitelogo,
+      card1,
+      card2,
+      card3,
+      card4,
+      card5,
+      card6,
+     linkedin,
+     instagram,
+     youtube
+     
+}
+export const serviceData = [
+     { icon: assets.logo, title: 'Detect', description: 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Dolorum eius aliquid quo aspernatur et rerum error odit aut, beatae, suscipit autem, voluptates corporis quidem quod quas quam fuga ipsam magnam?' },
+     { icon: assets.logo, title: 'Protect', description: 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Dolorum eius aliquid quo aspernatur et rerum error odit aut, beatae, suscipit autem, voluptates corporis quidem quod quas quam fuga ipsam magnam?' },
+     { icon: assets.logo, title: 'Respond', description: 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Dolorum eius aliquid quo aspernatur et rerum error odit aut, beatae, suscipit autem, voluptates corporis quidem quod quas quam fuga ipsam magnam?'},
+]
+ 
