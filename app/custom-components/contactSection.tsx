2afab26ded6d7261ddd83837818f374569f05348
sujaykumar <82111261+sujaykumar007@users.noreply.github.com>
2025-04-22 13:56:22 +0530

    add a directory

--- app/custom-components/contactSection.tsx
+++ app/custom-components/contactSection.tsx
@@ -43,7 +43,7 @@ export default function ContactUs() {
     }
   
     try {
-      const res = await fetch('/api/contact', {
+      const res = await fetch('/api/email', {
         method: 'POST',
         headers: { 'Content-Type': 'application/json' },
         body: JSON.stringify({
