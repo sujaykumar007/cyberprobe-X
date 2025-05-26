f6de36abe535958fe19a8a3df7254d2ff59d8392
Anikethanshetty <shettyanikethand@gmail.com>
2025-05-26 19:41:24 +0530

    added to config file

--- next.config.ts
+++ next.config.ts
@@ -1,9 +1,33 @@
-import type { NextConfig } from "next";
+const securityHeaders = [
+  {
+    key: 'Content-Security-Policy',
+    value: `default-src 'self'; script-src 'self'; style-src 'self' 'unsafe-inline'; img-src 'self' data:; font-src 'self'; connect-src 'self'; object-src 'none'; frame-ancestors 'none'; base-uri 'self';`
+  },
+  {
+    key: 'X-Frame-Options',
+    value: 'DENY' 
+  },
+  {
+    key: 'X-Content-Type-Options',
+    value: 'nosniff'
+  },
+  {
+    key: 'Referrer-Policy',
+    value: 'strict-origin-when-cross-origin'
+  },
+  {
+    key: 'Permissions-Policy',
+    value: 'camera=(), microphone=(), geolocation=()' // disable permissions
+  }
+]
 
-const nextConfig: NextConfig = {
-  /* config options here */
-};
-
-export default nextConfig;
-
-
+module.exports = {
+  async headers() {
+    return [
+      {
+        source: '/(.*)',
+        headers: securityHeaders,
+      },
+    ]
+  },
+}
